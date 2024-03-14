8c65cb232a590c396eb6bb39378b6951c5d31664
Sara <sarareyvalino23@gmail.com>
2024-03-14 13:52:51 +0100

    .

--- proyecto_base/proj/lib/intereses/formaciones.dart
+++ proyecto_base/proj/lib/intereses/formaciones.dart
@@ -1,13 +1,20 @@
-import 'package:flutter/material.dart';
+
 
 class Formacion{
-  final int id;
+   final int id;
   final String descripcion;
   final String requisitos;
-  final DateTime fecha;
-  final DateTime fin;
+  final DateTime fechaInicio;
+  final DateTime fechaFin;
   final int coste;
-  final Image imagen;
+  final String imagen;
+  final DateTime fecha;
 
-  Formacion(this.id, this.descripcion, this.requisitos, this.fecha, this.fin, this.coste, this.imagen);
+
+
+  Formacion(this.id, this.descripcion, this.requisitos, this.fechaInicio, this.fechaFin, this.coste, this.imagen, this.fecha);
+
+
+
+
 }
