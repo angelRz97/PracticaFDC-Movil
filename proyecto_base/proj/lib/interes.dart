0766d85c5dc8f90bbc1cd864b2803325567ea8b9
Caanosa <danisc2003@gmail.com>
2024-03-14 12:17:33 +0100

    Actualizacion Carlos

---
+++ proyecto_base/proj/lib/interes.dart
@@ -0,0 +1,13 @@
+class Interes {
+  final int id;
+  final String nombre;
+
+  const Interes({required this.id, required this.nombre});
+}
+
+const listaIntereses = [
+  Interes(id: 1, nombre: 'Java'),
+  Interes(id: 2, nombre: 'Flutter'),
+  Interes(id: 3, nombre: 'C++'),
+  Interes(id: 4, nombre: 'JavaScript')
+];
