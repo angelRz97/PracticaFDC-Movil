c91ae9f998801d0eabee6c23429e2d3d26d22655
Carlos Iglesias Ramos <121090555+carlosiglram@users.noreply.github.com>
2024-03-15 13:51:09 +0100

    Actualizacion proyecto general

---
+++ proyecto_base/proj/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'paginas/login.dart';
+import 'paginas/principal.dart';
+
+void main() {
+  runApp(const MyApp(
+    usuario: '',
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final String usuario;
+  const MyApp({Key? key, required this.usuario});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, initialRoute: '/login', routes: {
+      "/login": (BuildContext context) => Login(),
+      '/principal': (BuildContext context) => Principal(usuario: usuario),
+    });
+  }
+}
