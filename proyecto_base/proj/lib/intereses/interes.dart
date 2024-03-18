7280ebe813d9119003861ab9c244aa5f570565e0
Caanosa <danisc2003@gmail.com>
2024-03-18 11:33:45 +0100

    modificaciones

--- proyecto_base/proj/lib/intereses/interes.dart
+++
@@ -1,9 +0,0 @@
-// ignore: empty_constructor_bodies
-class Interest {
-  final int id;
-  final String name;
-
-  const Interest({required this.id, required this.name});
-}
-
-const allInterests = [Interest(id: 1, name: 'Java'), Interest(id: 2, name: 'Flutter'), Interest(id: 3, name: 'C++'), Interest(id: 4, name: 'JavaScript')];
